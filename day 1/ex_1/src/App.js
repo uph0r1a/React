3cbeba92a5afae5289427f664c03b69475dc0ce4
uph0r1a <tdtri1712@gmail.com>
2025-12-09 21:14:52 +0700

    a

---
+++ day 1/ex_1/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  console.log("App");
+  const username = "Tri";
+  
+  return (
+    <div className="App">
+      <h1>username</h1>
+    </div>
+  );
+}
+
+export default App;
