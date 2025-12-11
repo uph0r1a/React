1979b779792d5de32a9072b2e9e880c798faf4b8
uph0r1a <tdtri1712@gmail.com>
2025-12-11 18:27:43 +0700

    a

--- day 1/ex_1/src/App.js
+++ day 1/ex_1/src/App.js
@@ -2,15 +2,40 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-
-  console.log("App");
-  const username = "Tri";
+  const person = {
+    name: "Thai Duc Tri",
+    age: 19,
+    job: "HS",
+    hobbies: ["Gameing", "Coding", "Hanging out"]
+  };
 
   return (
-    <div className="App">
-      <h1>Hello, {username}</h1>
+    <div style={styles.container}>
+      <h1>Personal info</h1>
+
+      <p><strong>Name:</strong> {person.name}</p>
+      <p><strong>Age:</strong> {person.age}</p>
+      <p><strong>Job:</strong> {person.job}</p>
+
+      <strong>Hobbies:</strong>
+      <ul>
+        {person.hobbies.map((hobby, index) => (
+          <li key={index}>{hobby}</li>
+        ))}
+      </ul>
     </div>
   );
 }
+
+const styles = {
+  container: {
+    padding: "20px",
+    fontFamily: "Arial",
+    backgroundColor: "#f4f4f4",
+    width: "350px",
+    borderRadius: "10px",
+    margin: "20px auto"
+  }
+};
 
 export default App;
